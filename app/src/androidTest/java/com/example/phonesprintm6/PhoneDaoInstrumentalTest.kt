f0914fd902c12a491df3b4f14c32618dbdf7fa9d
Roberto Medina P <robertomedinamv@gmail.com>
2023-08-08 23:36:39 -0400

    con 2 pruebas unitarias

---
+++ app/src/androidTest/java/com/example/phonesprintm6/PhoneDaoInstrumentalTest.kt
@@ -0,0 +1,82 @@
+package com.example.phonesprintm6
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.phonesprintm6.Model.Local.Database.PhoneDataBase
+import com.example.phonesprintm6.Model.Local.Entitties.PhoneDetailEntity
+import com.example.phonesprintm6.Model.Local.Entitties.PhoneEntity
+import com.example.phonesprintm6.Model.Local.PhoneDao
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//Todo Check it out
+@RunWith(AndroidJUnit4::class)
+//@Config(sdk=[Build.VERSION_CODES.Q], manifest= Config.NONE)
+class PhoneDaoInstrumentalTest {
+
+    private lateinit var phoneDao: PhoneDao
+    private lateinit var db: PhoneDataBase
+
+    @Before
+    fun setup(){
+        val context= ApplicationProvider.getApplicationContext<android.content.Context>()
+        db= Room.inMemoryDatabaseBuilder(context,PhoneDataBase::class.java).build()
+        phoneDao=db.getPhoneDao()
+    }
+
+    @After
+    fun shutDown(){
+        db.close()
+    }
+
+    @Test
+    fun insertListPhones()= runBlocking {
+        val phoneEntity= listOf(
+            PhoneEntity(1,"pruena",1,"prueba imagen"),
+            PhoneEntity(2,"prueba 2",2,"prueba imagen 2")
+        )
+
+        val phoneLivedata= phoneDao.getAllPhones()
+        val phoneList: List<PhoneEntity> = phoneLivedata.value?: emptyList()
+
+        //verificar el listado
+
+        assertThat(phoneList, not(emptyList()))
+        assertThat(phoneList.size,equalTo(2))
+
+    }
+
+    @Test
+    fun insertPhoneDetail()= runBlocking{
+
+        val phoneDetail= PhoneDetailEntity(
+            2,
+            "nombre insertado",
+            2,
+            "imagen2",
+            "descripcion2",
+            2,
+            true
+        )
+
+        phoneDao.insertPhoneDetail(phoneDetail)
+        val phoneLiveData= phoneDao.getPhoneDetailById("2")
+        val phoneValue= phoneLiveData.value
+
+        assertThat(phoneValue?.id, equalTo("2"))
+        assertThat(phoneValue?.image, equalTo("imagen2"))
+
+
+    }
+
+
+
+
+}
